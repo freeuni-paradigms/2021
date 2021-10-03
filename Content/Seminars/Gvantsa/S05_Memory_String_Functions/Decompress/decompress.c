79793670aa71b61c84e4d5c985d7350efe0e5162
Gvantsa <gtsutskhashvili@gmail.com>
2021-10-03 12:36:57 +0400

    Add S05-S06

--- Content/Seminars/Gvantsa/S05_Memory_String_Functions/Decompress/decompress.c
+++ Content/Seminars/Gvantsa/S05_Memory_String_Functions/Decompress/decompress.c
@@ -5,21 +5,162 @@
 void Decompress(char **data)
 {
     char *src = *data;
-    char *str = "";
+    char *result = NULL;
+    int size = 0;
     while (*src != '\0')
     {
-        // realloc(src, size)
-        int byte = ...;
-        int length = ...;
-        int count  = ...;
+        int byte = *(unsigned char *)src;
+        int length = byte >> 4;
+        int count = byte & 0xF; // byte & ((1 << 4) - 1) // byte - (length << 4)
         src++;
-        char* sub_str = (src, src+length);
-        for (int i = 0; i < count; i++){
-            str += sub_str;
+        result = realloc(result, size + count * length);
+        for (int i = 0; i < count; i++)
+        {
+            char *dest = result + size + i * length;
+            strncpy(dest, src, length);
         }
         src += length;
+        size += count * length;
     }
-    str += "\0";
+    result = realloc(result, size + 1);
+    result[size] = '\0';
     free(*data);
-    *data = str;
+    *data = result;
+}
+
+void Test1()
+{
+    printf("Test1: ");
+    char *expectValue = "aabcbcbcx";
+    char *data = malloc(8);
+    char *ptr = data;
+
+    // 00010010|a|00100011|b|c|00010001|x
+    *ptr = (1 << 4) + (1 << 1); // 00010010
+    ptr++;
+    *ptr = 'a';
+    ptr++;
+    *ptr = (1 << 5) + (1 << 1) + 1; // 00100011
+    ptr++;
+    *ptr = 'b';
+    ptr++;
+    *ptr = 'c';
+    ptr++;
+    *ptr = (1 << 4) + 1; // 00010001
+    ptr++;
+    *ptr = 'x';
+    ptr++;
+    *ptr = '\0';
+
+    Decompress(&data);
+
+    if (strcmp(expectValue, data) == 0)
+    {
+        printf("PASSED\n");
+    }
+    else
+    {
+        printf("FAILED after decompress: %s expected: %s\n", data, expectValue);
+    }
+    free(data);
+}
+
+void Test2()
+{
+    printf("Test2: ");
+    char *expectValue = "yfoofoobar";
+    char *data = malloc(11);
+    char *ptr = data;
+
+    // 00010001|y|00110010|f|o|o|00110001|b|a|r
+    *ptr = (1 << 4) + 1; // 00010001
+    ptr++;
+    *ptr = 'y';
+    ptr++;
+    *ptr = (1 << 5) + (1 << 4) + (1 << 1); // 00110010
+    ptr++;
+    *ptr = 'f';
+    ptr++;
+    *ptr = 'o';
+    ptr++;
+    *ptr = 'o';
+    ptr++;
+    *ptr = (1 << 5) + (1 << 4) + 1; // 00110001
+    ptr++;
+    *ptr = 'b';
+    ptr++;
+    *ptr = 'a';
+    ptr++;
+    *ptr = 'r';
+    ptr++;
+    *ptr = '\0';
+
+    Decompress(&data);
+
+    if (strcmp(expectValue, data) == 0)
+    {
+        printf("PASSED\n");
+    }
+    else
+    {
+        printf("FAILED after decompress: %s expected: %s\n", data, expectValue);
+    }
+    free(data);
+}
+
+void Test3()
+{
+    printf("Test3: ");
+    char *expectValue = "abcdefghfoofoo";
+    char *data = malloc(14);
+    char *ptr = data;
+
+    // 10000001|a|b|c|d|e|f|g|h|00110010|f|o|o
+    *ptr = (1 << 7) + 1; // 10000001
+    ptr++;
+    *ptr = 'a';
+    ptr++;
+    *ptr = 'b';
+    ptr++;
+    *ptr = 'c';
+    ptr++;
+    *ptr = 'd';
+    ptr++;
+    *ptr = 'e';
+    ptr++;
+    *ptr = 'f';
+    ptr++;
+    *ptr = 'g';
+    ptr++;
+    *ptr = 'h';
+    ptr++;
+    *ptr = (1 << 5) + (1 << 4) + (1 << 1); // 00110010
+    ptr++;
+    *ptr = 'f';
+    ptr++;
+    *ptr = 'o';
+    ptr++;
+    *ptr = 'o';
+    ptr++;
+    *ptr = '\0';
+
+    Decompress(&data);
+
+    if (strcmp(expectValue, data) == 0)
+    {
+        printf("PASSED\n");
+    }
+    else
+    {
+        printf("FAILED after decompress: %s expected: %s\n", data, expectValue);
+    }
+    free(data);
+}
+
+int main()
+{
+    Test1();
+    Test2();
+    Test3();
+    return 0;
 }
--- Content/Seminars/Gvantsa/S06_Generics/Scheme/scheme.c
+++ Content/Seminars/Gvantsa/S06_Generics/Scheme/scheme.c
@@ -2,8 +2,34 @@
 
 char *ConcatStrings(const char *first, const char *second)
 {
+    // S05/Strings/concat_strings.c
 }
 
 char *ConcatAll(nodeType *list)
 {
+    nodeType current = *list;
+
+    switch (current)
+    {
+    case Nil:
+    case Integer:
+        return strdup("");
+    case String:
+        return strdup((char *)(list + 1));
+    }
+
+    nodeType **ptr = list + 1;
+
+    nodeType *first = ptr[0]; // *(ptr + 0) // *((char *)ptr + 0 * sizeof(nodeType *))
+    nodeType *second = ptr[1];
+
+    char *firstStr = ConcatAll(first);
+    char *secondStr = ConcatAll(second);
+
+    char *result = ConcatStrings(firstStr, secondStr);
+
+    free(firstStr);
+    free(secondStr);
+
+    return result;
 }
