f97dc529c659c943d7687e0bc6792eb63b6424a0
Gvantsa <gtsutskhashvili@gmail.com>
2021-10-03 15:57:08 +0400

    Add S07

---
+++ Content/Seminars/Gvantsa/S07_Generics_Sorted_Set/README.md
@@ -0,0 +1,10 @@
+---
+nav_order: 7
+title: 7. ჯენერიკები - Sorted Set
+parent: გვანცა
+grand_parent: სემინარები
+---
+
+# გამოყენებული კოდები
+
+## [პირობა](https://github.com/freeuni-paradigms/2021/tree/master/Content/Seminars/Gvantsa/S07_Generics_Sorted_Set/problem.pdf)
---
+++ Content/Seminars/Gvantsa/S07_Generics_Sorted_Set/sortedset.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct
+{
+} sortedset;
+
+void SetNew(sortedset *set, int elemSize, int (*cmpfn)(const void *, const void *));
+bool SetAdd(sortedset *set, const void *elemPtr);
+void *SetSearch(sortedset *set, const void *elemPtr);
