4f19fd851f194e399a1166375349df8aa9517ab0
Giorgi Lekveishvili <giolekva@gmail.com>
2021-10-05 16:38:28 +0400

    stack - free fn

--- Content/Lectures/L06-01/stack.c
+++ Content/Lectures/L06-01/stack.c
@@ -4,18 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-void StackInit(Stack* s, int elem_size) {
+void StackInit(Stack* s, int elem_size,
+	       FreeFn free_fn) {
   s->elem_size = elem_size;
   s->log_len = 0;
   s->alloc_len = 4;
   s->base = malloc(s->alloc_len * elem_size);
   assert(s->base != NULL);
+  s->free_fn = free_fn;
 }
 
 void StackDestroy(Stack* s) {
+  if (s->free_fn != NULL) {
+    for (int i = 0; i < s->log_len; i++) {
+      s->free_fn((char*)s->base + s->log_len * s->elem_size);
+    }
+  }
   free(s->base);
 }
 
+// Takes ownership of memory at value.
 void StackPush(Stack* s, void* value) {
   if (s->log_len == s->alloc_len) {
     s->alloc_len *= 2;
@@ -26,6 +34,7 @@ void StackPush(Stack* s, void* value) {
   s->log_len++;
 }
 
+// Transfers ownership back to the client.
 void StackPop(Stack* s, void* out) {
   assert(s->log_len > 0);
   s->log_len--;
--- Content/Lectures/L06-01/stack.h
+++ Content/Lectures/L06-01/stack.h
@@ -1,14 +1,22 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
+typedef void(*FreeFn)(void*);
+ReturnType(*FunctionTypeName)(Arg1Type, Arg2Type, ...)
+void(void*) FreeFn
+
+typedef definition name;
+
 typedef struct {
   void* base;
   int elem_size;
   int log_len;
   int alloc_len;
+  FreeFn free_fn;
 } Stack;
 
-void StackInit(Stack* s, int elem_size);
+void StackInit(Stack* s, int elem_size,
+	       FreeFn free_fn);
 void StackDestroy(Stack* s);
 void StackPush(Stack* s, void* value);
 void StackPop(Stack* s, void* out);
