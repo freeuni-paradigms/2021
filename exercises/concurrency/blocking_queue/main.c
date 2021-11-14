98518c9a9d503f4cab38e6eee0113bbf8ea9e7d3
Gvantsa <gtsutskhashvili@gmail.com>
2021-11-14 19:27:12 +0400

    add S17

--- exercises/concurrency/blocking_queue/main.c
+++ exercises/concurrency/blocking_queue/main.c
@@ -1,4 +1,5 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -8,21 +9,57 @@
 
 typedef struct
 {
-
+    vector vec;
+    sem_t write;
+    sem_t read;
+    sem_t lock;
+    size_t elemSize;
 } Queue;
 
-void init_queue(Queue *q)
+void init_queue(Queue *q, int elemSize, VectorFreeFunction freefn)
 {
+    q->elemSize = elemSize;
+
+    VectorNew(&q->vec, elemSize, freefn, 1);
+
+    sem_init(&q->write, 0, QUEUESIZE);
+    sem_init(&q->read, 0, 0);
+    sem_init(&q->lock, 0, 1);
 }
 
 void enqueue(Queue *q, void *elem)
 {
+    sem_wait(&q->write);
+
+    sem_wait(&q->lock);
+    VectorAppend(&q->vec, elem);
+    sem_post(&q->lock);
+
+    sem_post(&q->read);
 }
 
 void dequeue(Queue *q, void **ptr)
 {
+    sem_wait(&q->read);
+
+    sem_wait(&q->lock);
+    void *elem = VectorNth(&q->vec, 0);
+    void *newElem = malloc(q->elemSize);
+    memcpy(newElem, elem, q->elemSize);
+    *ptr = newElem;
+    VectorDelete(&q->vec, 0);
+    sem_post(&q->lock);
+
+    sem_post(&q->write);
 }
 
 void destroy_queue(Queue *q)
 {
+    VectorDispose(&q->vec);
+
+    sem_destroy(&q->write);
+    sem_destroy(&q->read);
+    sem_destroy(&q->lock);
+
+    free(q);
 }
--- exercises/concurrency/race_simulation/main.c
+++ exercises/concurrency/race_simulation/main.c
@@ -19,7 +19,46 @@ void RandomSleep()
     }
 }
 
+typedef struct
+{
+    int id;
+    int *place;
+    sem_t *lock;
+} data_T;
+
+void *race(void *datat)
+{
+    data_T *data = (data_T *)datat;
+    for (int i = 0; i < DISTANCE; i++)
+        RandomSleep();
+
+    int id = data->id;
+    sem_wait(data->lock);
+    int place = *(data->place);
+    *(data->place) = place + 1;
+    sem_post(data->lock);
+    printf("Racer %d: Place %d\n", id, place);
+}
+
 int main()
 {
     srand(time(NULL));
+
+    int *place = malloc(sizeof(int));
+    *place = 1;
+
+    sem_t *lock = malloc(sizeof(sem_t));
+    sem_init(lock, 0, 1);
+
+    pthread_t racers[NUM_RACERS];
+    for (int i = 0; i < NUM_RACERS; i++)
+    {
+        data_T *data = malloc(sizeof(data_T));
+        data->id = i + 1;
+        data->place = place;
+        data->lock = lock;
+        pthread_create(&racers[i], NULL, race, data);
+    }
+
+    pthread_exit(NULL);
 }
