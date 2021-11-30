9c9660396ca2909bc95355a75ca6913758b36218
gvantsats <gtsutskhashvili@gmail.com>
2021-11-30 13:27:50 +0400

    add solutions

--- exercises/scheme/basics/solution.scm
+++ exercises/scheme/basics/solution.scm
@@ -1,22 +1,37 @@
 (define (first-elem lst)
-    -1
+    (if (null? lst)
+        "None"
+        (car lst)
+    )
 )
 
+
 (define (abs_ n)
-    -1
+    (if (positive? n)
+        n
+        (- 0 n)
+    )
 )
 
 
 (define (right-triangle lst)
-    -1
+    (equal? (+ (expt (car lst) 2) (expt (cadr lst) 2))
+        (expt (caddr lst) 2)
+    )
 )
 
 
 (define (last-elem lst)
-    -1
+    (cond
+        ((null? lst) "None")
+        ((null? (cdr lst)) (car lst))
+        (else (last-elem (cdr lst)))
+    )
 )
 
 
 (define (length_ lst)
-    -1
+    (if (null? lst) 0
+        (+ 1 (length_ (cdr lst)))
+    )
 )
--- exercises/scheme/exercises/tests.scm
+++ exercises/scheme/exercises/tests.scm
@@ -8,9 +8,11 @@
 (display (if (equal? (filter-range '(1 2 3 4) 9 14) '()) "4 SUCCESS" "4 FAILURE"))(newline)
 (display (if (equal? (filter-range '(1 2 3 4) 4 1) '(1 2 3 4)) "5 SUCCESS" "5 FAILURE"))(newline)
 
+
 (display (if (equal? (prime? 2) #t) "6 SUCCESS" "6 FAILURE"))(newline)
 (display (if (equal? (prime? 12) #f) "7 SUCCESS" "7 FAILURE"))(newline)
 (display (if (equal? (prime? 1) #f) "8 SUCCESS" "8 FAILURE"))(newline)
+
 
 (display (if (equal? (primes? '(1 2 3 4)) '(#f #t #t #f)) "9 SUCCESS" "9 FAILURE"))(newline)
 (display (if (equal? (primes? '()) '()) "10 SUCCESS" "10 FAILURE"))(newline)
