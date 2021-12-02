4844786f38788ef1791627fcf224816030d55f5a
gvantsats <gtsutskhashvili@gmail.com>
2021-12-02 11:30:57 +0400

    add new exercises

--- exercises/scheme/exercises/solution.scm
+++ exercises/scheme/exercises/solution.scm
@@ -13,3 +13,15 @@
 (define (max_divisible3 lst)
     0
 )
+
+(define (range k)
+    '()
+)
+
+(define (pow n)
+    0
+)
+
+(define (generate_powers k)
+    '()
+)
--- exercises/scheme/exercises/tests.scm
+++ exercises/scheme/exercises/tests.scm
@@ -14,4 +14,10 @@
 (display (if (equal? (max_divisible3 '(4 8)) 0) "8 SUCCESS" "8 FAILURE"))(newline)
 (display (if (equal? (max_divisible3 '(33 48 22 102)) 102) "9 SUCCESS" "9 FAILURE"))(newline)
 
+(display (if (equal? (range 5) '(1 2 3 4 5)) "10 SUCCESS" "10 FAILURE"))(newline)
+
+(display (if (equal? (pow 5) 32) "11 SUCCESS" "11 FAILURE"))(newline)
+(display (if (equal? (pow 1) 2) "12 SUCCESS" "12 FAILURE"))(newline)
+
+(display (if (equal? (generate_powers 4) '(2 4 8 16)) "13 SUCCESS" "13 FAILURE"))(newline)
 
