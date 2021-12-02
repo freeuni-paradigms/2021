54c5cf5c10210b178d548d6143598c2f04720387
gvantsats <gtsutskhashvili@gmail.com>
2021-12-02 11:20:06 +0400

    add S22

--- exercises/scheme/exercises/solution.scm
+++ exercises/scheme/exercises/solution.scm
@@ -1,43 +1,15 @@
-(define (k-th-element lst k)
-    (cond
-        ((null? lst) "None")
-        ((zero? k) (car lst))
-        (else (k-th-element (cdr lst) (- k 1)))
-    )
+(define (reverse_ lst)
+    '()
 )
 
-(define (filter-range-helper lst a b)
-    (cond 
-        ((null? lst) '())
-        ((and (<= a (car lst)) (<= (car lst) b)) 
-            (cons (car lst) (filter-range (cdr lst) a b)))
-        (else (filter-range (cdr lst) a b))
-    )
+(define (max_ lst)
+    0
 )
 
-(define (filter-range lst a b)
-    (if (> a b)
-        (filter-range-helper lst b a)
-        (filter-range-helper lst a b)
-    )
+(define (filter_ func lst)
+    '()
 )
 
-(define (prime-helper n k)
-    (cond 
-        ((> k (sqrt n)) #t)
-        ((zero? (remainder n k)) #f)
-        (else (prime-helper n (+ k 1)))
-    )
-)
-
-(define (prime? n)
-    (if (= n 1) #f
-        (prime-helper n 2)
-    )
-)
-
-(define (primes? lst)
-    (if (null? lst) '()
-        (cons (prime? (car lst)) (primes? (cdr lst))) 
-    )
+(define (max_divisible3 lst)
+    0
 )
--- exercises/scheme/exercises/tests.scm
+++ exercises/scheme/exercises/tests.scm
@@ -1,20 +1,17 @@
-(load "PATH/solution.scm")
+(load "solution.scm")
 
-(display (if (equal? (k-th-element '(1 2 3) 2) 3) "1 SUCCESS" "1 FAILURE"))(newline)
-(display (if (equal? (k-th-element '() 0) "None") "2 SUCCESS" "2 FAILURE"))(newline)
-
-
-(display (if (equal? (filter-range '(1 2 3 4) 2 4) '(2 3 4)) "3 SUCCESS" "3 FAILURE"))(newline)
-(display (if (equal? (filter-range '(1 2 3 4) 9 14) '()) "4 SUCCESS" "4 FAILURE"))(newline)
-(display (if (equal? (filter-range '(1 2 3 4) 4 1) '(1 2 3 4)) "5 SUCCESS" "5 FAILURE"))(newline)
+(display (if (equal? (reverse_ '()) '()) "1 SUCCESS" "1 FAILURE"))(newline)
+(display (if (equal? (reverse_ '(1 2 3)) '(3 2 1)) "2 SUCCESS" "2 FAILURE"))(newline)
 
 
-(display (if (equal? (prime? 2) #t) "6 SUCCESS" "6 FAILURE"))(newline)
-(display (if (equal? (prime? 12) #f) "7 SUCCESS" "7 FAILURE"))(newline)
-(display (if (equal? (prime? 1) #f) "8 SUCCESS" "8 FAILURE"))(newline)
+(display (if (equal? (max_ '()) 0) "3 SUCCESS" "3 FAILURE"))(newline)
+(display (if (equal? (max_ '(4 5 3)) 5) "4 SUCCESS" "4 FAILURE"))(newline)
 
+(display (if (equal? (filter_ positive? '(1 -2 45 -10 -20)) '(1 45)) "5 SUCCESS" "5 FAILURE"))(newline)
+(display (if (equal? (filter_ odd? '(4 5 3)) '(5 3)) "6 SUCCESS" "6 FAILURE"))(newline)
 
-(display (if (equal? (primes? '(1 2 3 4)) '(#f #t #t #f)) "9 SUCCESS" "9 FAILURE"))(newline)
-(display (if (equal? (primes? '()) '()) "10 SUCCESS" "10 FAILURE"))(newline)
-(display (if (equal? (primes? '(10 12 23 33)) '(#f #f #t #f)) "11 SUCCESS" "11 FAILURE"))(newline)
+(display (if (equal? (max_divisible3 '(4 9 3)) 9) "7 SUCCESS" "7 FAILURE"))(newline)
+(display (if (equal? (max_divisible3 '(4 8)) 0) "8 SUCCESS" "8 FAILURE"))(newline)
+(display (if (equal? (max_divisible3 '(33 48 22 102)) 102) "9 SUCCESS" "9 FAILURE"))(newline)
+
 
