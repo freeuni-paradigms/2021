c48ff7eb35b4df5988cba9dca865b70e9c2256b9
gvantsats <gtsutskhashvili@gmail.com>
2021-12-06 23:31:37 +0400

    add exercises

---
+++ exercises/scheme/map/solution.scm
@@ -0,0 +1,27 @@
+(define (digits n)
+    '()
+)
+
+(define (list_power lst)
+    '()
+)
+
+(define (sum lst)
+    0
+)
+
+(define (narcisisstic? n)
+    #f
+)
+
+(define (count n lst)
+    0
+)
+
+(define (set lst)
+    '()
+)
+
+(define (frequency lst)
+    '()
+)
