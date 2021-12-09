b7b446b6a356e2c24877050785e0c8b9914d5fc9
dpipi17 <dpipi17@freeuni.edu.ge>
2021-12-09 12:06:11 +0400

    Scheme let problems init

---
+++ exercises/scheme/let/solution.scm
@@ -0,0 +1,19 @@
+;1 reverse duplicate
+(define (reverseDup lst) 
+    '()
+)
+
+;2. binpow
+(define (binpow a b)
+    0
+)
+
+;3. fibonacci
+(define (fib n)
+    0
+)
+
+;4. list partition
+(define (partition n lst)
+    '()
+)
