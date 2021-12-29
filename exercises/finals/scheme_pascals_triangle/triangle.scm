5264294ac4044e77988ef35549160936b4f5e216
dpipi17 <dpipi17@freeuni.edu.ge>
2021-12-30 00:37:25 +0400

    students.asm and pascal triangle solutions

--- exercises/finals/scheme_pascals_triangle/triangle.scm
+++ exercises/finals/scheme_pascals_triangle/triangle.scm
@@ -1,4 +1,23 @@
+; (define (helper lst)
+;   (if (null? (cdr lst)) '()
+;     (cons (+ (car lst) (cadr lst)) (helper (cdr lst)))
+;   )
+; )
+
+; (define (row index)
+;   (if
+;     (equal? index 1) '(1)
+;     (append (list 1) (helper (row (- index 1))) (list 1))
+;   )
+; )
+
+(define (find-next-row prev-row)
+  (map + (cons 0 prev-row) (append prev-row (list 0)))
+)
+
 (define (row index)
-  ; IMPLEMENTATION
-  '()
+  (if
+    (equal? index 1) '(1)
+    (find-next-row (row (- index 1)))
+  )
 )
